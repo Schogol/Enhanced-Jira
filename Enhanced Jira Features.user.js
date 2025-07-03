8ce348a637de2313dcfe8696d820954291bf9b66
Tulwarr <tulwar@eve-isd.net>
2025-07-03 20:18:38 +0200

    Update Enhanced Jira Features.user.js

--- Enhanced Jira Features.user.js
+++ Enhanced Jira Features.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        Enhanced Jira Features
 // @version     2.6.4
-// @author      ISD BH Schogol
+// @author      ISD BH Schogol 
 // @description Adds a Translate, Assign to GM, Convert to Defect and Close button to Jira and also parses Log Files submitted from the EVE client
 // @updateURL   https://github.com/Schogol/Enhanced-Jira/raw/main/Enhanced%20Jira%20Features.user.js
 // @downloadURL https://github.com/Schogol/Enhanced-Jira/raw/main/Enhanced%20Jira%20Features.user.js
